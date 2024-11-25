2b8e647554f7fe83eea890d41ad4e13fb671b909
Sohyunxxi <dlthgus15780@gmail.com>
2024-11-25 19:23:49 +0900

    chore: add database configuration with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MypageModule } from './mypage/mypage.module';
 import { VolunteersModule } from './volunteers/volunteers.module';
 import { ReviewsModule } from './reviews/reviews.module';
@@ -7,6 +9,17 @@ import { RecruitModule } from './recruit/recruit.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     MypageModule,
     VolunteersModule,
     ReviewsModule,
